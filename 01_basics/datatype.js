b29561ac766dcf6009d4018a2a210865910f3da5
Harshita Sarma <101857398+Sarma1234@users.noreply.github.com>
2026-07-06 15:43:52 +0000

    config done

--- 01_basics/datatype.js
+++ 01_basics/datatype.js
@@ -2,19 +2,19 @@
 // 7 types:  String, number, boolean, null, undefined, symbol
 //, BigInt
 
-const score = 100
-const scoreValue = 100.3
+// const score = 100
+// const scoreValue = 100.3
 
-const isLoggedIn = false
-const OutsideTemp = null
-let userEmail;
+// const isLoggedIn = false
+// const OutsideTemp = null
+// let userEmail;
 
-const id = Symbol('123')
-const anotherId = Symbol('123')
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
 
-console.log(id === anotherId);
+// console.log(id === anotherId);
 
-const bigNumber =  34565435766541392873n
+//const bigNumber =  34565435766541392873n
 
 
 
@@ -24,16 +24,35 @@ const bigNumber =  34565435766541392873n
 //Non Primitives
 
 //Arrays, Objects, Function
-const heros = ["Shaktiman", "naagraj", "doga"];
-let myObj = {
-    name: "hitesh",
-    age: 22,
+// const heros = ["Shaktiman", "naagraj", "doga"];
+// let myObj = {
+//     name: "hitesh",
+//     age: 22,
 
+// }
+
+// const myFunction = function(){
+//     console.log("Hello World");
+
+// }
+
+// console.log(typeof bigNumber);
+
+//++++++++++++++++++++++++++++++++++++++++++++++++
+// stack(primitive),heap(non primitive)
+
+let myName = "HarshitaSarma"
+let anothername = myName
+anothername = "chaiaurcode"
+
+console.log(anothername);
+console.log(myName);
+
+let userOne  = {
+    email: "user@gmail.com",
+    upi: "user@ybl"
 }
-
-const myFunction = function(){
-    console.log("Hello World");
-
-}
-
-console.log(typeof bigNumber);
+let userTwo = userOne
+userTwo.email = "harshita@gmail.com"
+console.log(userOne.email);
+console.log(userTwo.email);
