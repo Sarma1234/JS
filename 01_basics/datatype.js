eaef520c377b8786e0b0d42a0ef3814a463a5750
Harshita Sarma <101857398+Sarma1234@users.noreply.github.com>
2026-07-06 15:22:06 +0000

    config done

---
+++ 01_basics/datatype.js
@@ -0,0 +1,39 @@
+// Primitive
+// 7 types:  String, number, boolean, null, undefined, symbol
+//, BigInt
+
+const score = 100
+const scoreValue = 100.3
+
+const isLoggedIn = false
+const OutsideTemp = null
+let userEmail;
+
+const id = Symbol('123')
+const anotherId = Symbol('123')
+
+console.log(id === anotherId);
+
+const bigNumber =  34565435766541392873n
+
+
+
+
+
+
+//Non Primitives
+
+//Arrays, Objects, Function
+const heros = ["Shaktiman", "naagraj", "doga"];
+let myObj = {
+    name: "hitesh",
+    age: 22,
+
+}
+
+const myFunction = function(){
+    console.log("Hello World");
+
+}
+
+console.log(typeof bigNumber);
